abed989dd48bc3ff609d9552a2fbde25f19e06d9
fstipo <fstipanov@gmail.com>
2022-11-12 10:49:38 +0100

    folder structure

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,10 +1,10 @@
 import Moment from "moment";
 import axios from "axios";
 
-// DATE FORMAT
+// * DATE FORMAT
 export const dateFormat = (date) => Moment(date).format('DD.MM.YYYY, h:mm:ss A');
 
-// IN moment
+// * IN moment
 export const getData = (id, date, fn) => {
     const url = `https://es-demo.azurewebsites.net/v1/People/${id}/history?inMoment=${date}`
     axios(url)
@@ -15,7 +15,7 @@ export const getData = (id, date, fn) => {
         .catch(err => console.error(err))
 }
 
-// SELECT DATES
+// * SELECT DATES
 export const getSelectDatesData = (id, startDate, endDate, fn) => {
     const url = `https://es-demo.azurewebsites.net/v1/People/${id}/history?from=${startDate}&until=${endDate}`
     axios(url)
